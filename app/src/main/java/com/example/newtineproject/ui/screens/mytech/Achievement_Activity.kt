bcd31a49fd56af72e13c5a8766bd668f7f717008
oh-bom <spdlqj888888@gmail.com>
2023-08-06 14:24:20 +0900

    REBASE

--- app/src/main/java/com/example/newtineproject/ui/screens/mytech/Achievement_Activity.kt
+++ app/src/main/java/com/example/newtineproject/ui/screens/mytech/Achievement_Activity.kt
@@ -1,16 +1,13 @@
-package com.example.newtineproject
+package com.example.newtineproject.ui.screens.mytech
 
 import android.app.DatePickerDialog
-import android.graphics.Rect
-import android.os.Build
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,9 +15,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -30,28 +30,25 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.newtineproject.R
 import com.example.newtineproject.ui.theme.LightBlue
 import com.example.newtineproject.ui.theme.NewTineProjectTheme
 import java.time.DayOfWeek
@@ -68,7 +65,8 @@ class Achievement_Activity : ComponentActivity() {
             NewTineProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize()
+                        .background(Color.White),
                     color = Color.White
                 ) {
                     AchievementScreen()
@@ -113,9 +111,12 @@ fun AchievementScreen() {
         )
 
     Scaffold(
+        modifier = Modifier.background(Color.White),
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
                 title = { Text("뉴테크") },
+                modifier = Modifier.background(
+                    Color.White),
                 navigationIcon = {
                     IconButton(
                         onClick = { /* Handle back navigation */ }
@@ -137,10 +138,12 @@ fun AchievementScreen() {
                 modifier = Modifier
                     .padding(20.dp)
                     .fillMaxSize()
+                    .background(Color.White)
             ) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
+                        .wrapContentHeight()
                         .padding(top = 30.dp)
                 ) {
                     // 습관 달성 현황
@@ -150,11 +153,14 @@ fun AchievementScreen() {
                         color = Color.Black,
                         textAlign = TextAlign.Start,
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .wrapContentWidth()
                             .padding(top = 20.dp)
                     )
 
+                    Spacer(modifier =Modifier.weight(1f))
+
                     IconButton(
+                        modifier = Modifier.padding(top=15.dp),
                         onClick = {
                             datePicker.show()
                         }
@@ -175,8 +181,8 @@ fun AchievementScreen() {
                     modifier = Modifier
                         .fillMaxWidth()
                         .background(Color.White)
-                        .clip(RoundedCornerShape(16.dp))
-                        .padding(top = 5.dp, bottom = 5.dp, start = 5.dp, end = 5.dp)
+                        .border(1.dp, Color.Gray, RoundedCornerShape(5.dp))
+                        .padding(top = 5.dp, bottom = 5.dp, start = 10.dp, end = 10.dp)
 
                 ){
                     LazyVerticalGrid(
@@ -209,12 +215,35 @@ fun AchievementScreen() {
                         .padding(top = 10.dp),
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
+
+                    var cnt=1
                     repeat(3) {
-                        Image(
-                            painter = painterResource(id = R.drawable.blue_check),
-                            contentDescription = "Checkmark Icon",
-                            modifier = Modifier.size(72.dp)
-                        )
+                        Box(modifier = Modifier
+                            .background(Color.White)
+                            .height(72.dp)
+                            .width(72.dp)
+                            .border(1.dp, LightBlue, RoundedCornerShape(50.dp))
+                        ){
+                            Text(text = "$cnt",
+                                fontSize = 30.sp,
+                                modifier = Modifier
+                                    .padding(10.dp)
+                                    .align(Alignment.Center)
+                                ,
+                                color= LightBlue
+                            )
+
+
+                        }
+                        cnt+=1
+
+
+                        //완료시 바뀌는 이미지
+//                        Image(
+//                            painter = painterResource(id = R.drawable.blue_check),
+//                            contentDescription = "Checkmark Icon",
+//                            modifier = Modifier.size(72.dp)
+//                        )
                     }
                 }
 
@@ -251,13 +280,16 @@ fun AchievementScreen() {
                     modifier = Modifier.fillMaxWidth()
                 )
 
+                Spacer(modifier = Modifier.weight(1f))
+
                 // Reward Button
                 Button(
                     onClick = { /* Handle reward button click */ },
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 10.dp,bottom=10.dp)
+                        .padding(top = 10.dp, bottom = 10.dp)
                         .background(LightBlue)
+
                     ,
                     colors = ButtonDefaults.buttonColors(LightBlue)
 
@@ -296,7 +328,7 @@ fun getThisWeekMap():MutableMap<String,String>{
         val dateString=date.format(formatter)
         val dayOfWeek=date.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.KOREA)
 
-        weekMap.put(dateString,dayOfWeek.substring(0,1))
+        weekMap.put(dayOfWeek.substring(0,1),dateString)
     }
     return weekMap
 }
