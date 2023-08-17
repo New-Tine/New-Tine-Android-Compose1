8ca5e7daa42e68b2e3ca8f8303e8b7fd52625447
unknown <world2222222222@gmail.com>
2023-08-17 14:37:38 +0900

    REFACTOR: Put images on buttons
    Put all the images on the buttons. It's now completed.

--- app/src/main/java/com/example/newtineproject/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/newtineproject/ui/screens/home/HomeScreen.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -29,12 +31,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -66,9 +71,7 @@ fun HomeScreen(
 
             Spacer(modifier = Modifier.height(15.dp))
 
-            TodayNewTechWithLogo()
-
-            ReadCountBox() // 파라미터에 몇 개 읽었는지 읽을 수 있도록 나중에 설정
+            TodayNewTechAndReadCountBoxWithCoins()
 
             Spacer(modifier = Modifier.height(10.dp))
 
@@ -126,16 +129,18 @@ fun ButtonsBelowTopBar(navController: NavHostController) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 17.dp, top = 10.dp, end = 17.dp, bottom = 4.dp),
+            .padding(start = 17.dp, end = 17.dp, bottom = 4.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         ButtonOf(
             buttonText = "습관 설정",
+            buttonIcon = painterResource(id = R.drawable.logo_calendar_1),
             navController = navController,
             destination = HomeDetailScreen.HabitSetting.route
         )
         ButtonOf(
             buttonText = "실시간 토론",
+            buttonIcon = painterResource(id = R.drawable.logo_debate_1),
             navController = null,
             destination = ""
         )
@@ -145,6 +150,7 @@ fun ButtonsBelowTopBar(navController: NavHostController) {
 @Composable
 fun ButtonOf(
     buttonText: String,
+    buttonIcon: Painter,
     navController: NavHostController?,
     destination: String
 ) {
@@ -153,10 +159,11 @@ fun ButtonOf(
             onClick = { navController?.navigate(destination) },
             modifier = Modifier
                 .width(170.dp)
-                .height(55.dp),
+                .height(65.dp)
+                .padding(top = 10.dp),
             colors = ButtonDefaults.buttonColors(HomeButtonBelowTopBarColor),
             shape = RoundedCornerShape(5.dp),
-            contentPadding = PaddingValues(start = 50.dp)
+            contentPadding = PaddingValues(start = 55.dp)
         ) {
             Text(
                 text = buttonText,
@@ -166,10 +173,38 @@ fun ButtonOf(
                 )
             )
         }
-//        Image(
-//            painter = painterResource(id = R.drawable.button_habitsetting),
-//            contentDescription = "habitsetting_icon"
-//        )
+        Image(
+            modifier = Modifier.size(65.dp),
+            painter = buttonIcon,
+            contentDescription = "Logo Calendar"
+        )
+    }
+}
+
+@Composable
+fun TodayNewTechAndReadCountBoxWithCoins() {
+    Box {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            TodayNewTechWithLogo()
+            Spacer(modifier = Modifier.height(2.dp))
+            ReadCountBox()
+        }
+        Box(modifier = Modifier.offset(x = 260.dp, y = 30.dp)) {
+            Image(
+                modifier = Modifier.size(47.dp),
+                painter = painterResource(id = R.drawable.coin_home_1),
+                contentDescription = "coin_1"
+            )
+            Image(
+                modifier = Modifier
+                    .size(83.dp)
+                    .offset(x = 1.dp, y = 4.dp),
+                painter = painterResource(id = R.drawable.coin_home_2),
+                contentDescription = "coin_2"
+            )
+        }
     }
 }
 
@@ -192,14 +227,15 @@ fun TodayNewTechWithLogo() {
             painter = painterResource(id = R.drawable.newtine_logo),
             contentDescription = "newtine logo",
             modifier = Modifier
-                .width(44.dp)
-                .padding(top = 4.dp)
+                .size(50.dp)
+                .offset(y = 3.dp)
         )
     }
 }
 
 @Composable
 fun ReadCountBox() {
+    // 파라미터에 몇 개 읽었는지 읽을 수 있도록 나중에 설정
     Row(
         modifier = Modifier
             .fillMaxWidth()
